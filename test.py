a7990ebf0bd2b581612a8de9c31ddf0082276c59
PRANAV <pranav@PRANAVs-MacBook-Air.local>
2026-06-15 08:47:09 +0530

    test file saved

---
+++ test.py
@@ -0,0 +1,5 @@
+print("This is a test file for Git practice.")
+
+print("We are learning how to use Git for version control.")
+
+print("Git allows us to track changes and collaborate with others on code projects.")
