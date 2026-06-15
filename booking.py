69360b01e95e6e2caabead357aaa1ec18448df8b
PRANAV <pranav@PRANAVs-MacBook-Air.local>
2026-06-15 08:39:52 +0530

    Travel location code is updated

---
+++ booking.py
@@ -0,0 +1,5 @@
+print("Hello, World! from booking team")
+
+print("We are working on the booking system for our travel agency.")
+
+print("Our booking system will allow customers to easily book flights, hotels, and rental cars.")
