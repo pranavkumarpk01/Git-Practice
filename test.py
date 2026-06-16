0fbe50a1f32e051abdaeeb8fa04ed25130a665aa
PRANAV <pranav@PRANAVs-MacBook-Air.local>
2026-06-16 08:05:55 +0530

    test.py added a new line

--- test.py
+++ test.py
@@ -3,3 +3,5 @@ print("This is a test file for Git practice.")
 print("We are learning how to use Git for version control.")
 
 print("Git allows us to track changes and collaborate with others on code projects.")
+
+print("This file will be used to demonstrate basic Git commands and workflows.")
